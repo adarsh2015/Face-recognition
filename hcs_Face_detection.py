a18f9000d34bbaeeeceb09cefc1d0f98104604aa
adarsh2015 <bhargav.adarsh@yahoo.com>
2020-09-20 10:40:55 +0530

    Update hcs_Face_detection.py

--- hcs_Face_detection.py
+++ hcs_Face_detection.py
@@ -4,10 +4,10 @@ cap  = cv2.VideoCapture(0)
 while(True):
     
     ret,frame = cap.read()
-    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     if(ret==False):
         continue
-    faces  = face_cascade.detectMultiScale(frame,1.05,5)
+    faces  = face_cascade.detectMultiScale(frame,1.3,5)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),1)
     cv2.imshow("video frame",frame)
